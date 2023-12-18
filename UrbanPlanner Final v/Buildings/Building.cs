195f5efe91cc52ecd8ddfe6a68912dcef360b7e4
BenJarrett <b.cl.jarrett@gmail.com>
2023-12-18 13:25:54 -0500

    Created properites, some private fields, and two constructors.

---
+++ UrbanPlanner Final v/Buildings/Building.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner_Final_v.Buildings
+{
+    internal class Building
+    {
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public int Volume { get; }
+
+
+
+        string _designer = "Ben Jarrett";
+        DateTime _dateConstructed = DateTime.Now;
+        string _address = "800 8th Street";
+        string _owner;
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public Building(string address, int stories, double width, double depth) : this(address)
+        {
+            Stories = stories;
+            Width = width;
+            Depth = depth;
+
+        }
+
+    }
+}
