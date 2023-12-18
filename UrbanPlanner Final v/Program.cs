92cbe3147874ce1b883948facc314d36ced6d385
BenJarrett <b.cl.jarrett@gmail.com>
2023-12-18 12:42:05 -0500

    Changing program file to match school resources format

--- UrbanPlanner Final v/Program.cs
+++ UrbanPlanner Final v/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace UrbanPlanner_Final_v
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
