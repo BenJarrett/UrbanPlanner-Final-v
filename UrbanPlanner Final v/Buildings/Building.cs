b98c2cef9f2130cf2ff51e7414cbac4854c2de25
BenJarrett <b.cl.jarrett@gmail.com>
2023-12-19 18:58:38 -0500

    Added functionality foe building the city.

--- UrbanPlanner Final v/Buildings/Building.cs
+++ UrbanPlanner Final v/Buildings/Building.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,9 +17,9 @@ namespace UrbanPlanner_Final_v.Buildings
 
 
         string _designer = "Ben Jarrett";
-        DateTime _dateConstructed = DateTime.Now;
-        string _address = "800 8th Street";
-        string _owner;
+        DateTime _dateConstructed; 
+        string _address;
+        string _owner = "";
 
         public Building(string address)
         {
@@ -32,6 +33,41 @@ namespace UrbanPlanner_Final_v.Buildings
             Depth = depth;
             Volume = (int)(width * depth * (3 * stories));
 
+        }
+
+        public void Construct()
+        {
+            
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string owner )
+        {
+            _owner = owner;
+        }
+
+        //public void Print()
+        //{
+        //    Console.WriteLine(_designer + "---------");
+        //    Console.WriteLine(_owner);
+        //    Console.WriteLine(_dateConstructed);
+        //    Console.WriteLine(_owner);
+        //    Console.WriteLine(_address);
+
+        //}
+
+        public static void PrintBuildings(List<Building> buildings)
+        {
+            foreach (var building in buildings)
+            {
+                Console.WriteLine($"{building._address} -------------------- ");
+                Console.WriteLine($"Designed By {building._designer}");
+                Console.WriteLine($"Construded on {building._dateConstructed}");
+                Console.WriteLine($"Owned by {building._owner}");
+                Console.WriteLine($"{building.Volume} cubic meters of space");
+                Console.WriteLine("");
+
+            }
         }
 
 
--- UrbanPlanner Final v/Program.cs
+++ UrbanPlanner Final v/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UrbanPlanner_Final_v.Buildings;
 
 namespace UrbanPlanner_Final_v
 
@@ -9,9 +10,34 @@ namespace UrbanPlanner_Final_v
         static void Main(string[] args)
         {
 
+            var buildingOne = new Building("512 8th Avenue", 4, 36, 34);
+            var buildingTwo = new Building("64 18th St.", 2, 22, 16);
+            var buildingThree = new Building("4506 Hatfield St.", 3, 16, 24);
+
+            buildingOne.Construct();
+            buildingTwo.Construct();
+            buildingThree.Construct();
+
+            buildingOne.Purchase("Bob B");
+            buildingTwo.Purchase("Carlos D");
+            buildingThree.Purchase("Michelle O");
+
+            var buildingsList = new List<Building>
+            {
+                buildingOne, buildingTwo, buildingThree
+            };
 
 
+            Building.PrintBuildings(buildingsList);
+            //var allBuildings = new Building { buildingOne, buildingTwo, buildingThree };
+
+            //buildingInstance.PrintBuildings(buildingsList);
+
+            //buildingOne.Print();
+            //buildingTwo.Print();
+            //buildingThree.Print();
 
         }
+        
     }
 }
